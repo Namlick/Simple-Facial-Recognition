e5f2a3c916b4913f3ce4cd909cb1e91d82aef704
Nicholas Amely <92178377+Namlick@users.noreply.github.com>
2023-04-06 18:27:10 -0700

    Update headshots_picam.py

--- headshots_picam.py
+++ headshots_picam.py
@@ -2,7 +2,7 @@ import cv2
 from picamera import PiCamera
 from picamera.array import PiRGBArray
 
-name = 'Caroline' #replace with your name
+name = 'your_name' #replace with your name
 
 cam = PiCamera()
 cam.resolution = (512, 304)
@@ -17,13 +17,13 @@ while True:
         cv2.imshow("Press Space to take a photo", image)
         rawCapture.truncate(0)
     
-        k = cv2.waitKey(1)
+        key = cv2.waitKey(1)
         rawCapture.truncate(0)
-        if k%256 == 27: # ESC pressed
+        if key == ord("q"): # q key pressed
             break
-        elif k%256 == 32:
+        elif key%256 == 32:
             # SPACE pressed
-            img_name = "dataset/"+ name +"/image_{}.jpg".format(img_counter)
+            img_name = "Datasets/"+ name +"/image_{}.jpg".format(img_counter)
             cv2.imwrite(img_name, image)
             print("{} written!".format(img_name))
             img_counter += 1
